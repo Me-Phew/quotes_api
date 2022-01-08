4aed91b7fa2d2cd4fac8430ed90de56f36b231c8
Me-Phew <basmateusz@wp.pl>
2022-01-08 21:42:58 +0100

    Add accessed at column

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import TIMESTAMP, ARRAY
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 
@@ -10,5 +10,6 @@ class Quote(Base):
     content = Column(String, nullable=False)
     author = Column(String, nullable=False)
     language = Column(String, nullable=False)
+    times_accessed = Column(Integer, nullable=False, server_default=text('0'))
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
